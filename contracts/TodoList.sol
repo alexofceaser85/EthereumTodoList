4bc5062c8bf78485a9b29ef54a37ac0be8b6f12b
Alex <contact@alexdecesare.com>
2022-03-27 14:02:12 -0400

    Added the todo list class
    Added the class for the todo list item

---
+++ contracts/TodoList.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TodoList {
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        addTask("Hello World");
+    }
+
+    function addTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+}
